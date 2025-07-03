697d978949564d71ee33efc2206b1d2ba98c6384
TiYab <pari.run@gmail.com>
2025-07-03 09:16:40 +0400

    ajout appli

--- app.py
+++ app.py
@@ -22,3 +22,18 @@ def home():
             error = f"Erreur lors de l'appel API : {str(e)}"
 
     return render_template('index.html', reco=reco, user_id=user_id, top_k=top_k, error=error)
+
+
+from flask import jsonify
+
+@app.route('/api/reco')
+def api_reco():
+    user_id = request.args.get('user_id', 15)
+    top_k = request.args.get('top_k', 5)
+    try:
+        url = f"https://mvpreco.azurewebsites.net/api/reco?user_id={user_id}&top_k={top_k}"
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())  # renvoie JSON au frontend
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
